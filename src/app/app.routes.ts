3afcab70f997836124a2ab9a9a81677e79db8318
Martin Alejandro Bonari <131064127+martinmab2@users.noreply.github.com>
2026-05-15 02:24:43 -0300

    Enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Puntouno } from './components/puntouno/puntouno';
+import { Puntodos } from './components/puntodos/puntodos';
+import { Puntotres } from './components/puntotres/puntotres';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'puntouno', component: Puntouno },
+  { path: 'puntodos', component: Puntodos },
+  { path: 'puntotres', component: Puntotres },
+  { path: '**', redirectTo: '' }
+];
